251dceaa629ae31fe72b284f878fb6532f37457c
hyejeong seo <seohj92@podbbang.com>
2018-06-28 12:32:02 +0900

    Make Model and Controller

---
+++ FaceIt/FaceIt/FacialExpression.swift
@@ -0,0 +1,49 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by hyejeong seo on 2018. 6. 28..
+//  Copyright © 2018년 hyejeong seo. All rights reserved.
+//
+
+import Foundation
+
+struct FacialExpression {
+    enum Eyes: Int {
+        case Open
+        case Closed
+        case Squinting
+    }
+    
+    enum EyeBrows: Int {
+        case Relaxed
+        case Normal
+        case Furrowed
+        
+        func moreRelaxedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue - 1) ?? .Relaxed
+        }
+        func moreFurrowedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue + 1) ?? .Furrowed
+        }
+    }
+    
+    enum Mouth: Int {
+        case Frown
+        case Smirk
+        case Neutral
+        case Grin
+        case Smile
+        
+        func sadderMouth() -> Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .Frown
+        }
+        func happierMouth() -> Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .Smile
+        }
+    }
+    
+    var eyes: Eyes
+    var eyeBrows: EyeBrows
+    var mouth: Mouth
+}
