12b092e7240fcbf9ac6a7c94a2c527e61c6b7ea1
hyejeong seo <seohj92@podbbang.com>
2018-06-29 10:39:20 +0900

    아이폰, 아이패드 대응

---
+++ FaceIt/FaceIt/EmotionsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by hyejeong seo on 2018. 6. 28..
+//  Copyright © 2018년 hyejeong seo. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+    
+    private let emotionalFaces: Dictionary<String, FacialExpression> = [
+        "angry" : FacialExpression(eyes: .Closed, eyeBrows: .Furrowed, mouth: .Frown),
+        "happy" : FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile),
+        "worried" : FacialExpression(eyes: .Open, eyeBrows: .Relaxed, mouth: .Smirk),
+        "mischievous" : FacialExpression(eyes: .Open, eyeBrows: .Furrowed, mouth: .Grin)
+    ]
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destinationvc = segue.destination
+        if let navcon = destinationvc as? UINavigationController {
+            destinationvc = navcon.visibleViewController ?? destinationvc
+        }
+        if let facevc = destinationvc as? FaceViewController {
+            if let identifier = segue.identifier {
+                if let expression = emotionalFaces[identifier] {
+                    facevc.expression = expression
+                    if let sendingButton = sender as? UIButton {
+                        facevc.navigationItem.title = sendingButton.currentTitle
+                    }
+                }
+            }
+        }
+    }
+    
+}
